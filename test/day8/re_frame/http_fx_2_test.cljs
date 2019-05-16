f69a7ca47525b34eddab885ae3c2465d43d6e45e
Isaac Johnston <isaac@superstruct.nz>
2019-05-16 21:11:55 +0800

    Add test/day8/re_frame/http_fx_2_test.cljs

---
+++ test/day8/re_frame/http_fx_2_test.cljs
@@ -0,0 +1,9 @@
+(ns day8.re-frame.http-fx-2-test
+  (:require
+    [clojure.test :refer [deftest is testing async use-fixtures]]
+    [clojure.spec.alpha :as s]
+    [re-frame.core :as re-frame]
+    [day8.re-frame.http-fx-2]))
+
+(deftest placeholder
+  (is true))
