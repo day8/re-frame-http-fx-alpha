b515e93736efdb68b238391627f0f8829cd5a53c
Isaac Johnston <isaac@superstruct.nz>
2019-11-04 23:59:48 +0800

    Reformat project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject day8.re-frame/http-fx-2 "see :git-version below https://github.com/arrdem/lein-git-version"
+(defproject    day8.re-frame/http-fx-2 "see :git-version below https://github.com/arrdem/lein-git-version"
   :description "A re-frame effects handler for fetching resources (including across the network)."
-  :url "https://github.com/day8/re-frame-http-fx-2.git"
-  :license {:name "MIT"}
+  :url         "https://github.com/day8/re-frame-http-fx-2.git"
+  :license     {:name "MIT"}
 
   :git-version
   {:status-to-version
@@ -15,19 +15,19 @@
              patch+           (inc patch)]
          (format "%s.%d-%s-SNAPSHOT" prefix patch+ ahead))))}
 
-  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+  :dependencies [[org.clojure/clojure       "1.10.1" :scope "provided"]
                  [org.clojure/clojurescript "1.10.520" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library]]
-                 [thheller/shadow-cljs "2.8.62" :scope "provided"]
-                 [re-frame "0.10.9" :scope "provided"]]
+                 [thheller/shadow-cljs      "2.8.62" :scope "provided"]
+                 [re-frame                  "0.10.9" :scope "provided"]]
 
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
-                                  [karma-reporter "3.1.0"]]
-                   :plugins      [[lein-shell "0.5.0"]]}}
+                                  [karma-reporter     "3.1.0"]]}}
 
   :plugins [[me.arrdem/lein-git-version "2.0.3"]
-            [lein-shadow "0.1.5"]]
+            [lein-shadow                "0.1.5"]
+            [lein-shell                 "0.5.0"]]
 
   :clean-targets [:target-path
                   "shadow-cljs.edn"
@@ -35,12 +35,11 @@
                   "package-lock.json"
                   "resources/public/js/test"]
 
-  :resource-paths ["run/resources"]
   :jvm-opts ["-Xmx1g"]
 
   :source-paths ["src"]
-
   :test-paths ["test"]
+  :resource-paths ["run/resources"]
 
   :shadow-cljs {:nrepl  {:port 8777}
 
